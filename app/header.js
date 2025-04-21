e38e089183bf7e7f8c1a39b2d4978dfde09e54cb
blazbabsek <blaz.babsek@gmail.com>
2025-04-21 20:54:46 +0200

    vadim

---
+++ app/header.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+export default function Header() {
+  return (
+<header class="header">
+
+
+    <nav class="nav">
+      <a href="#">Domov</a>
+      <a href="#">O Nas</a>
+      <a href="#">Zgodovina</a>
+      <a href="#">Aktualno</a>
+      <a href="#">Kontakt</a>
+    </nav>
+  </header>
+  )
+  }
